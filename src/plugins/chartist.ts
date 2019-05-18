ae80de86c89bad208fe39e91e594923c7b9ba3d1
Devildude4427 <ryanchristian4427@gmail.com>
2019-05-18 19:07:07 +0100

    Converted the plugins to all be TypeScript files.

---
+++ src/plugins/chartist.ts
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import 'chartist/dist/chartist.min.css';
+
+import vue_chartist from 'vue-chartist';
+
+Vue.use(vue_chartist);
--- src/plugins/index.js
+++
@@ -1,5 +0,0 @@
-import ApiService from './api.axios'
-import './chartist'
-import './vuetify'
-
-ApiService.init();
